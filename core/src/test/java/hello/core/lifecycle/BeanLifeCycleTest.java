c0d5033becbf33dcee8fba41c3846aa4f2490da1
seokmin <devhong0016@gmail.com>
2026-06-24 01:39:07 +0900

    @PostConstruct,@PreDestroy

--- core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -20,6 +20,8 @@ public class BeanLifeCycleTest {
 
     @Configuration
     static class LifeCycleConfig{
+
+//        @Bean(initMethod = "init", destroyMethod = "close")
         @Bean
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
@@ -27,7 +29,6 @@ public class BeanLifeCycleTest {
             return networkClient;
         }
     }
-
 
 
 }
--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,13 @@
 package hello.core.lifecycle;
+//
+//import org.springframework.beans.factory.DisposableBean;
+//import org.springframework.beans.factory.InitializingBean;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+//public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient  {
     private String url;
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
@@ -20,18 +24,33 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("call: " + url + " message = " + message);
     }
     //서비스 종료시 호출
-    public void disconnect() {
+    public void disConnect() {
         System.out.println("close: " + url);
     }
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    @PostConstruct
+    public void init() {
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
-        disconnect();
+    
+    @PreDestroy
+    public void close() {
+        System.out.println("NetworkClient.close");
+        disConnect();
     }
+
+
+
+//    @Override
+//    public void afterPropertiesSet() throws Exception {
+//        connect();
+//        call("초기화 연결 메시지");
+//    }
+//
+//    @Override
+//    public void destroy() throws Exception {
+//        disconnect();
+//    }
 }
