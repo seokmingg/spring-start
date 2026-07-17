31ae6be8cb69c7ed66e3e5de510b057c87e4b0c5
seokmin <devhong0016@gmail.com>
2026-07-17 22:50:28 +0900

    make DispatcherServlet5(frontcontrollerV5)

---
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -0,0 +1,70 @@
+package hello.servlet.web.frontcontroller.v5;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import hello.servlet.web.frontcontroller.v4.ControllerV4;
+import hello.servlet.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+@WebServlet(name = "frontControllerServletV5", urlPatterns = "/front-controller/v5/*")
+public class FrontControllerServletV5 extends HttpServlet {
+
+    private final Map<String, Object> handlerMappingMap = new HashMap<>();
+    private final List<MyHandlerAdapter> handlerAdapters = new ArrayList<>();
+
+    public FrontControllerServletV5(){
+        initHandlerMappingMap();
+        initHandlerAdapters();
+
+    }
+
+    private List<MyHandlerAdapter> getHandlerAdapters() {
+        return handlerAdapters;
+    }
+
+    private void initHandlerMappingMap() {
+        handlerMappingMap.put("/front-controller/v3/members/new-form",new MemberFormControllerV3());
+        handlerMappingMap.put("/front-controller/v3/members/save",new MemberSaveControllerV3());
+        handlerMappingMap.put("/front-controller/v3/members",new MemberListControllerV3());
+    }
+
+    private void initHandlerAdapters() {
+        getHandlerAdapters().add(new ControllerV3HandlerAdapter());
+    }
+
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+       Object handler =getHandler(request);
+        if (handler == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+//        MyHandlerAdapter adapter = getHandlerAdapter(handler);
+//        ModelView mv = adapter.handle(request, response, handler);
+//        MyView view = viewResolver(mv.getViewName());
+//        view.render(mv.getModel(), request, response);
+
+    }
+
+    private Object getHandler(HttpServletRequest request) {
+        String requestURI = request.getRequestURI();
+        return handlerMappingMap.get(requestURI);
+    }
+
+}
